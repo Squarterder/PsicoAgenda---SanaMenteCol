225b014784d90af558ca03c4ca6688f2f6677560
Squarterder <eduardoespitiar@gmail.com>
2026-05-15 23:58:51 -0500

    configuracion para render

--- psicoagenda/settings.py
+++ psicoagenda/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-psicoagenda-sanamentecol-2026-cambia-en-produccion'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-psicoagenda-sanamentecol-2026-cambia-en-produccion')
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
